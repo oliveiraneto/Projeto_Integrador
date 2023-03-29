2ccfd784f92a9521ba4c7e75404e959ccfc47d4c
oliveiraneto <sebastiaonetosilva18@gmail.com>
2023-03-29 15:29:15 -0300

    new file:   programacao_web/adicionar.html 	new file:   programacao_web/alimento_fundo.jpg 	new file:   programacao_web/index.html 	new file:   programacao_web/listagem.html 	new file:   programacao_web/style.css 	new file:   programacao_web/tabela.js

---
+++ programacao_web/tabela.js
@@ -0,0 +1,12 @@
+// Preenche a tabela com os dados dos alimentos cadastrados
+// Armazenar referências aos elementos HTML relevantes
+const formulario = document.getElementById("formulario");
+const tabela = document.getElementById("tabela");
+
+// Armazenar os alimentos cadastrados
+const alimentos = [
+  { nome: "Arroz", validade: "30/06/2023", quantidade: "2 kg" },
+  { nome: "Feijão", validade: "30/06/2023", quantidade: "1 kg" },
+  { nome: "Macarrão", validade: "30/06/2023", quantidade: "500 g" }
+];
+
